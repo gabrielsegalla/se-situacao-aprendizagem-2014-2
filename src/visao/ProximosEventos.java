24cf6a4080ba3950cf8b648ca9c6563740d40f32
Nildo Beppler Jr <Nildo Beppler Jr@Nildo>
2014-10-21 19:21:42 -0200

    Inclusão incial do projeto

---
+++ src/visao/ProximosEventos.java
@@ -0,0 +1,107 @@
+package visao;
+
+import java.awt.Color;
+import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.border.EmptyBorder;
+import javax.swing.table.DefaultTableModel;
+
+public class ProximosEventos extends JFrame {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+	private JTable table;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					ProximosEventos frame = new ProximosEventos();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public ProximosEventos() {
+		setTitle("Pr\u00F3ximos Eventos\r\n");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 650, 451);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+
+		JButton btnEventos = new JButton("Eventos");
+		btnEventos.setBounds(403, 19, 156, 23);
+		contentPane.add(btnEventos);
+
+		JButton button_1 = new JButton("Ranking\r\n");
+		button_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				Ranking r = new Ranking();
+				r.setVisible(true);
+
+				dispose();
+			}
+		});
+		button_1.setBounds(302, 19, 91, 23);
+		contentPane.add(button_1);
+
+		JButton button_2 = new JButton("Meu Perfil\r\n");
+		button_2.setBounds(201, 19, 91, 23);
+		contentPane.add(button_2);
+
+		JLabel label = new JLabel("Senai");
+		label.setFont(new Font("Tahoma", Font.BOLD, 40));
+		label.setBounds(10, 0, 146, 42);
+		contentPane.add(label);
+
+		JLabel label_1 = new JLabel("Educando");
+		label_1.setForeground(Color.GRAY);
+		label_1.setBounds(122, 28, 77, 14);
+		contentPane.add(label_1);
+
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(68, 174, 491, 181);
+		contentPane.add(scrollPane);
+
+		table = new JTable();
+		table.setModel(new DefaultTableModel(new Object[][] {
+				{ null, null, null, null }, { null, null, null, null },
+				{ null, null, null, null }, { null, null, null, null },
+				{ null, null, null, null }, { null, null, null, null },
+				{ null, null, null, null }, { null, null, null, null },
+				{ null, null, null, null }, { null, null, null, null }, },
+				new String[] { "Nome da Palestra", "Hora de Inicio",
+						"Hora de T\u00E9rmino", "Pontos" }));
+		table.getColumnModel().getColumn(0).setPreferredWidth(113);
+		table.getColumnModel().getColumn(2).setPreferredWidth(91);
+		scrollPane.setViewportView(table);
+
+		JLabel lblEventos = new JLabel("Eventos");
+		lblEventos.setFont(new Font("Code Light", Font.PLAIN, 30));
+		lblEventos.setBounds(232, 84, 186, 70);
+		contentPane.add(lblEventos);
+	}
+}
---
+++ src/visao/TelaUsuario.java
@@ -0,0 +1,112 @@
+package visao;
+
+import java.awt.Color;
+import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
+import javax.swing.border.EmptyBorder;
+
+public class TelaUsuario extends JFrame {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					TelaUsuario frame = new TelaUsuario();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public TelaUsuario() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 651, 420);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+
+		JLabel lblSenai = new JLabel("SENAI");
+		lblSenai.setForeground(new Color(0, 0, 0));
+		lblSenai.setFont(new Font("Tahoma", Font.BOLD, 30));
+		lblSenai.setBounds(10, 23, 98, 30);
+		contentPane.add(lblSenai);
+
+		JLabel lblEducando = new JLabel("Educando");
+		lblEducando.setForeground(Color.GRAY);
+		lblEducando.setBounds(107, 39, 64, 14);
+		contentPane.add(lblEducando);
+
+		JButton btnPerfil = new JButton("Perfil");
+		btnPerfil.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+
+			}
+		});
+		btnPerfil.setBounds(171, 35, 87, 23);
+		contentPane.add(btnPerfil);
+
+		JButton btnEventos = new JButton("Eventos");
+		btnEventos.setBounds(345, 35, 87, 23);
+		contentPane.add(btnEventos);
+
+		JButton btnRanking = new JButton("Ranking");
+		btnRanking.setBounds(258, 35, 87, 23);
+		contentPane.add(btnRanking);
+
+		JLabel lblNewLabel = new JLabel("FOTO");
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel.setBounds(10, 124, 98, 151);
+		contentPane.add(lblNewLabel);
+
+		JLabel lblNomeDoAluni = new JLabel("NOME DO ALUNO");
+		lblNomeDoAluni.setBounds(118, 165, 151, 14);
+		contentPane.add(lblNomeDoAluni);
+
+		JLabel lblNewLabel_1 = new JLabel("Email: ");
+		lblNewLabel_1.setBounds(118, 195, 46, 14);
+		contentPane.add(lblNewLabel_1);
+
+		JLabel lblNewLabel_2 = new JLabel("emaildoaluno@provedor.com");
+		lblNewLabel_2.setBounds(167, 195, 178, 14);
+		contentPane.add(lblNewLabel_2);
+
+		JLabel lblNewLabel_3 = new JLabel("Turma: ");
+		lblNewLabel_3.setBounds(118, 220, 46, 14);
+		contentPane.add(lblNewLabel_3);
+
+		JLabel lblNewLabel_4 = new JLabel("TURMA");
+		lblNewLabel_4.setBounds(177, 220, 168, 14);
+		contentPane.add(lblNewLabel_4);
+
+		JLabel lblCpf = new JLabel("CPF: ");
+		lblCpf.setBounds(125, 245, 46, 14);
+		contentPane.add(lblCpf);
+
+		JLabel label = new JLabel("000.000.000-00");
+		label.setBounds(171, 245, 123, 14);
+		contentPane.add(label);
+	}
+}
