6badfeaa6ee95beef1bf4b1d2d6991edb7ca4313
gabriel_segalla <grsegalla@hotmail.com>
2014-10-22 16:03:14 -0200

    Organização das Telas, Implementações das Imagens

--- src/visao/TelaUsuario.java
+++ src/visao/TelaUsuario.java
@@ -17,6 +17,8 @@ import javax.swing.JTextField;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
+import javax.swing.ImageIcon;
+import java.awt.Toolkit;
 
 public class TelaUsuario extends JFrame {
 
@@ -55,26 +57,17 @@ public class TelaUsuario extends JFrame {
 	 * Create the frame.
 	 */
 	public TelaUsuario() {
+		setTitle("SENAI EDUCANDO");
+		setIconImage(Toolkit.getDefaultToolkit().getImage("C:\\Users\\Gabriel Segalla\\git\\se-situacao-aprendizagem-2014-2\\Imagens\\iCON.png"));
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 641, 416);
+		setBounds(100, 100, 700, 550);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-
-		JLabel lblSenai = new JLabel("SENAI");
-		lblSenai.setForeground(new Color(0, 0, 0));
-		lblSenai.setFont(new Font("Tahoma", Font.BOLD, 30));
-		lblSenai.setBounds(10, 11, 98, 30);
-		contentPane.add(lblSenai);
-
-		JLabel lblEducando = new JLabel("Educando");
-		lblEducando.setForeground(Color.GRAY);
-		lblEducando.setBounds(105, 27, 64, 14);
-		contentPane.add(lblEducando);
 		
 		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
-		tabbedPane.setBounds(10, 77, 615, 293);
+		tabbedPane.setBounds(8, 79, 671, 427);
 		contentPane.add(tabbedPane);
 		
 		JPanel panel = new JPanel();
@@ -118,61 +111,64 @@ public class TelaUsuario extends JFrame {
 		
 		textField_3 = new JTextField();
 		textField_3.setColumns(10);
-		textField_3.setBounds(175, 193, 138, 20);
+		textField_3.setBounds(10, 361, 138, 20);
 		panel.add(textField_3);
 		
 		JLabel label = new JLabel("CPF");
 		label.setFont(new Font("Bebas Neue", Font.PLAIN, 18));
-		label.setBounds(175, 168, 68, 19);
+		label.setBounds(10, 336, 68, 19);
 		panel.add(label);
 		
 		textField_4 = new JTextField();
 		textField_4.setColumns(10);
-		textField_4.setBounds(175, 137, 138, 20);
+		textField_4.setBounds(10, 305, 138, 20);
 		panel.add(textField_4);
 		
 		JLabel lblEmail = new JLabel("Email:");
 		lblEmail.setFont(new Font("Bebas Neue", Font.PLAIN, 18));
-		lblEmail.setBounds(175, 112, 68, 19);
+		lblEmail.setBounds(10, 280, 68, 19);
 		panel.add(lblEmail);
 		
 		textField_5 = new JTextField();
 		textField_5.setColumns(10);
-		textField_5.setBounds(175, 81, 138, 20);
+		textField_5.setBounds(10, 249, 138, 20);
 		panel.add(textField_5);
 		
 		JLabel lblDataDeNascimento = new JLabel("Data de Nascimento:");
 		lblDataDeNascimento.setFont(new Font("Bebas Neue", Font.PLAIN, 18));
-		lblDataDeNascimento.setBounds(175, 56, 157, 19);
+		lblDataDeNascimento.setBounds(10, 224, 157, 19);
 		panel.add(lblDataDeNascimento);
 		
-		JLabel lblFoto = new JLabel("Foto");
-		lblFoto.setBounds(395, 9, 157, 178);
-		panel.add(lblFoto);
-		
 		JButton btnSalvar = new JButton("Salvar");
-		btnSalvar.setBounds(511, 231, 89, 23);
+		btnSalvar.setBounds(567, 365, 89, 23);
 		panel.add(btnSalvar);
 		
 		JButton btnAlterar = new JButton("Alterar");
-		btnAlterar.setBounds(412, 231, 89, 23);
+		btnAlterar.setBounds(470, 365, 89, 23);
 		panel.add(btnAlterar);
 		
-		JButton btnExcluir = new JButton("Excluir");
-		btnExcluir.setBounds(313, 231, 89, 23);
-		panel.add(btnExcluir);
+		JLabel label_7 = new JLabel("");
+		label_7.setIcon(new ImageIcon("C:\\Users\\Gabriel Segalla\\git\\se-situacao-aprendizagem-2014-2\\Imagens\\extra Perfil.png"));
+		label_7.setBounds(-14, -557, 1195, 1370);
+		panel.add(label_7);
 		
 		JPanel panel_1 = new JPanel();
 		tabbedPane.addTab("Ranking", null, panel_1, null);
 		panel_1.setLayout(null);
 		
 		JScrollPane scrollPane = new JScrollPane();
-		scrollPane.setBounds(138, 47, 313, 171);
+		scrollPane.setBounds(24, 73, 615, 239);
 		panel_1.add(scrollPane);
 		
 		table = new JTable();
 		table.setModel(new DefaultTableModel(
 			new Object[][] {
+				{null, null, null},
+				{null, null, null},
+				{null, null, null},
+				{null, null, null},
+				{null, null, null},
+				{null, null, null},
 				{null, null, null},
 				{null, null, null},
 				{null, null, null},
@@ -192,34 +188,56 @@ public class TelaUsuario extends JFrame {
 		scrollPane.setViewportView(table);
 		
 		JLabel label_1 = new JLabel("Nome:");
-		label_1.setBounds(107, 233, 46, 14);
+		label_1.setBounds(168, 335, 46, 14);
 		panel_1.add(label_1);
 		
 		textField_6 = new JTextField();
 		textField_6.setColumns(10);
-		textField_6.setBounds(154, 232, 181, 20);
+		textField_6.setBounds(215, 334, 181, 20);
 		panel_1.add(textField_6);
 		
 		JButton button = new JButton("Procurar");
-		button.setBounds(356, 229, 114, 23);
+		button.setBounds(417, 331, 114, 23);
 		panel_1.add(button);
 		
 		JLabel label_2 = new JLabel("Ranking");
-		label_2.setFont(new Font("Bebas Neue", Font.PLAIN, 30));
-		label_2.setBounds(251, 0, 114, 32);
+		label_2.setForeground(Color.ORANGE);
+		label_2.setFont(new Font("Bebas Neue", Font.PLAIN, 57));
+		label_2.setBounds(241, 11, 271, 51);
 		panel_1.add(label_2);
+		
+		JLabel label_5 = new JLabel("");
+		label_5.setIcon(new ImageIcon("C:\\Users\\Gabriel Segalla\\git\\se-situacao-aprendizagem-2014-2\\Imagens\\Extra Ranking.png"));
+		label_5.setBounds(-24, -26, 722, 455);
+		panel_1.add(label_5);
+		
+		JLabel label_8 = new JLabel("Ranking");
+		label_8.setForeground(Color.BLACK);
+		label_8.setFont(new Font("Bebas Neue", Font.PLAIN, 57));
+		label_8.setBounds(236, 11, 271, 51);
+		panel_1.add(label_8);
 		
 		JPanel panel_2 = new JPanel();
 		tabbedPane.addTab("Eventos", null, panel_2, null);
 		panel_2.setLayout(null);
 		
 		JScrollPane scrollPane_1 = new JScrollPane();
-		scrollPane_1.setBounds(10, 43, 590, 211);
+		scrollPane_1.setBounds(38, 89, 594, 237);
 		panel_2.add(scrollPane_1);
 		
 		table_1 = new JTable();
 		table_1.setModel(new DefaultTableModel(
 			new Object[][] {
+				{null, null, null, null},
+				{null, null, null, null},
+				{null, null, null, null},
+				{null, null, null, null},
+				{null, null, null, null},
+				{null, null, null, null},
+				{null, null, null, null},
+				{null, null, null, null},
+				{null, null, null, null},
+				{null, null, null, null},
 				{null, null, null, null},
 				{null, null, null, null},
 				{null, null, null, null},
@@ -241,9 +259,31 @@ public class TelaUsuario extends JFrame {
 		table_1.getColumnModel().getColumn(3).setPreferredWidth(95);
 		scrollPane_1.setViewportView(table_1);
 		
-		JLabel label_3 = new JLabel("Ranking");
-		label_3.setFont(new Font("Bebas Neue", Font.PLAIN, 30));
-		label_3.setBounds(243, 0, 114, 32);
-		panel_2.add(label_3);
+		JLabel label_6 = new JLabel("");
+		label_6.setIcon(new ImageIcon("C:\\Users\\Gabriel Segalla\\git\\se-situacao-aprendizagem-2014-2\\Imagens\\extra evento.png"));
+		label_6.setBounds(-26, 0, 716, 480);
+		panel_2.add(label_6);
+		
+		JLabel label_9 = new JLabel("SENAI EDUCANDO");
+		label_9.setForeground(Color.LIGHT_GRAY);
+		label_9.setFont(new Font("Bebas Neue", Font.PLAIN, 54));
+		label_9.setBounds(103, 2, 344, 66);
+		contentPane.add(label_9);
+		
+		JLabel lblSenai = new JLabel("SENAI EDUCANDO");
+		lblSenai.setForeground(Color.DARK_GRAY);
+		lblSenai.setFont(new Font("Bebas Neue", Font.PLAIN, 54));
+		lblSenai.setBounds(97, 2, 344, 66);
+		contentPane.add(lblSenai);
+		
+		JLabel label_4 = new JLabel("");
+		label_4.setIcon(new ImageIcon("C:\\Users\\Gabriel Segalla\\git\\se-situacao-aprendizagem-2014-2\\Imagens\\P.ICON.png"));
+		label_4.setBounds(0, -17, 146, 116);
+		contentPane.add(label_4);
+		
+		JLabel label_3 = new JLabel("");
+		label_3.setIcon(new ImageIcon("C:\\Users\\Gabriel Segalla\\git\\se-situacao-aprendizagem-2014-2\\Imagens\\plano de Fundo.jpg"));
+		label_3.setBounds(-16, -23, 726, 550);
+		contentPane.add(label_3);
 	}
 }
