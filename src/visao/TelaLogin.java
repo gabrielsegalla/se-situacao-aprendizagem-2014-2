24e47f760e66bab1ae12fc6aff515adde85a3669
marjory_pereira <marjory_pereira@SNFNS2EP13VM081.florianopolis.sc.senai.br>
2014-10-28 21:34:52 -0200

    Criado a tela de Login

---
+++ src/visao/TelaLogin.java
@@ -0,0 +1,62 @@
+package visao;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
+import java.awt.Font;
+
+public class TelaLogin extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField textField;
+	private JTextField textField_1;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					TelaLogin frame = new TelaLogin();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public TelaLogin() {
+		setTitle("LOGIN - SENAI EDUCANDO");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		textField = new JTextField();
+		textField.setBounds(111, 157, 218, 20);
+		contentPane.add(textField);
+		textField.setColumns(10);
+		
+		textField_1 = new JTextField();
+		textField_1.setBounds(111, 188, 218, 20);
+		contentPane.add(textField_1);
+		textField_1.setColumns(10);
+		
+		JLabel lblFoto = new JLabel("FOTO");
+		lblFoto.setFont(new Font("Tahoma", Font.PLAIN, 35));
+		lblFoto.setBounds(168, 56, 90, 46);
+		contentPane.add(lblFoto);
+	}
+}
