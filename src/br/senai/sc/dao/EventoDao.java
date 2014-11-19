dad48e3b7ffa273d4da20e30426385ddfd9da0e9
bruno_schmitt <bruno_schmitt@SNFNS2EP13VM083.florianopolis.sc.senai.br>
2014-11-19 20:41:08 -0200

    Dao

---
+++ src/br/senai/sc/dao/EventoDao.java
@@ -0,0 +1,73 @@
+package br.senai.sc.dao;
+
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+
+import br.senai.sc.entity.Evento;
+import br.senai.sc.entity.Usuario;
+import br.senai.sc.utils.JpaUtils;
+
+public class EventoDao {
+
+	private EntityManager entityManager;
+
+	public EventoDao() {
+		JpaUtils.getInstance().createEntityManagerFactory();
+		entityManager = JpaUtils.getInstance().getEntityManager();
+	}
+
+	public Evento salvar(Evento evento) {
+		entityManager.getTransaction().begin();
+		try {
+			if (evento.getId() == null) {
+				entityManager.persist(evento);
+			} else {
+				entityManager.merge(evento);
+			}
+			entityManager.getTransaction().commit();
+		} catch (Exception e) {
+			entityManager.getTransaction().rollback();
+			Logger.getLogger(EventoDao.class.getName()).log(Level.SEVERE,
+					null, e);
+		}
+		entityManager.close();
+		return evento;
+
+	}
+	
+	public void remover(int id){
+		try {
+			entityManager.getTransaction().begin();
+			Evento evento = entityManager.getReference(Evento.class, id);
+			entityManager.remove(evento);
+			entityManager.getTransaction().commit();
+		} catch (Exception e) {
+			entityManager.getTransaction().rollback();
+			Logger.getLogger(EventoDao.class.getName()).log(Level.SEVERE, null, e);
+		}
+		entityManager.close();
+	}
+	
+	public List<Usuario> listar() {
+		Query query = entityManager.createQuery("From Evento", Evento.class);
+		return query.getResultList();
+	}
+	
+	public Evento buscarPorId(int id) {
+		return entityManager.find(Evento.class, id);
+	}
+	
+	public List<Evento> buscarPorNome(String nome) {
+		Query query = entityManager.createQuery(
+				"From Evento u where u.nome LIKE :nome", Evento.class);
+		query.setParameter("nome", nome);
+		return query.getResultList();
+	}
+	
+	
+
+}
---
+++ src/br/senai/sc/dao/RankingDao.java
@@ -0,0 +1,32 @@
+package br.senai.sc.dao;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+
+import br.senai.sc.entity.Evento;
+import br.senai.sc.entity.Ranking;
+import br.senai.sc.utils.JpaUtils;
+
+public class RankingDao {
+	
+	private EntityManager entityManager;
+	
+	public RankingDao() {
+		JpaUtils.getInstance().createEntityManagerFactory();
+		entityManager = JpaUtils.getInstance().getEntityManager();
+	}
+	
+	public Ranking buscarPorId(int id) {
+		return entityManager.find(Ranking.class, id);
+	}
+	
+	public List<Ranking> buscarPorNome(String nome) {
+		Query query = entityManager.createQuery(
+				"From Ranking u where u.nome LIKE :nome", Ranking.class);
+		query.setParameter("nome", nome);
+		return query.getResultList();
+	}
+
+}
